a13ec4f8571c7354f178fd00d213a15aba0c585b
yusuketono <yusuke.tono@di-v.co.jp>
2019-07-16 17:25:11 +0900

    group

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ChatSpace
@@ -13,10 +11,7 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
     end
+      config.i18n.default_locale = :ja
   end
 end
