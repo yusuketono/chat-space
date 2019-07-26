012c3c5355b78257e5e3743c4be29f9117047f4b
yusuketono <yusuke.tono@di-v.co.jp>
2019-07-26 16:31:10 +0900

    3. フォームが送信されたら、イベントが発火するようにする

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,7 @@
 $(document).on('turbolinks:load', function() {
-  
+  $('#new_message').on(`submit`,function(e){
+    e.preventDefault();
+    console.log(this)
+    var message =new FormData(this);
+  })
  });
