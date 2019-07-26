f898b0196001d410952e58637e2834de7fd7318d
yusuketono <yusuke.tono@di-v.co.jp>
2019-07-26 16:53:27 +0900

    4.修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,13 +2,15 @@ $(document).on('turbolinks:load', function() {
   $('#new_message').on(`submit`,function(e){
     e.preventDefault();
     var message =new FormData(this);
+    // console.log(this)
     var url = $(this).attr('action')
     $.ajax({
       url:url,
       type:'POST',
-      datatype:`json`,
-      processData:false,
-      contentType:false
+      data: message,
+      dataType:`json`,
+      processData: false,
+      contentType: false
     })
-  })
-});
+   })
+ });
