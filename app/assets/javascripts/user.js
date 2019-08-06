82e3516391200cc5c72402b45c9eb6dac3f0d4a7
yusuketono <yusuke.tono@di-v.co.jp>
2019-08-06 15:53:25 +0900

    インクリメンタルサーチ後の実装のステップ

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,5 +1,6 @@
 $(document).on('turbolinks:load', function() { 
   var search = $("#user-search-result");
+
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
@@ -10,7 +11,16 @@ $(document).on('turbolinks:load', function() {
 
   function appendNoUser(user){
     var html = `<div class = "chat-group-user clearfix">${user}</div>`
-    search_list.append(html);
+    search.append(html);
+  }
+
+  function addUser(name,user_id) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id="chat-group-user-${user_id}">
+                  <input name="group[user_ids][]" type="hidden" value="${user_id}">
+                  <p class="chat-group-user__name">${name}</p>
+                  <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn data-user-id=${user_id} data-user-name=${name}"> 削除</a>
+                </div>`
+    $("#chat-group-users").append(html);
   }
 
   $('#user-search-field').on('input',function(e){
@@ -22,7 +32,6 @@ $(document).on('turbolinks:load', function() {
       data: { keyword: input },
       dataType: 'json',
     })
-
     .done(function(users) {    
       search.empty();
        if (users.length !== 0) {
@@ -39,5 +48,14 @@ $(document).on('turbolinks:load', function() {
     })
   }); 
 
-  
+  $("#user-search-result").on("click", ".chat-group-user__btn--add", function(e){
+    var name = $(this).data("user-name");
+    var user_id = $(this).data("user-id");
+    addUser(name,user_id);
+    $(this).parent().remove();
+  });
+
+  $("#chat-group-users").on("click", ".js-remove-btn", function(e){
+    $(this).parent().remove();
+  });
 });
