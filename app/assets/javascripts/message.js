b29b74d927bec6eadd72ad3be257aad6b166afce
yusuketono <yusuke.tono@di-v.co.jp>
2019-08-15 14:32:04 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function() {
   function buildHTML(message) {
-    var content = message.content
+    var content = (message.content) 
     var image = message.image ? `<img src= ${ message.image }>` : "";
     var html = `<div class="message" data-id="${message.id}">
                   <div class="message__upper">
@@ -20,8 +20,18 @@ $(document).on('turbolinks:load', function() {
                 </div>`
   return html;
   }
+
+  function scrollBottom(){
+    var target = $('.message').last();
+    var position = target.offset().top + $('.messages').scrollTop();
+    $('.messages').animate({
+      scrollTop: position
+    }, 300, 'swing');
+  };
+
   $('#new_message').on(`submit`,function(e){
     e.preventDefault();
+    console.log(this)
     var message = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
@@ -35,18 +45,39 @@ $(document).on('turbolinks:load', function() {
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('#new_message')[0].reset();  
-      var target = $('.message').last();
-      var position = target.offset().top + $('.messages').scrollTop();
-      $('.messages').animate({
-        scrollTop: position
-      }, 300, 'swing');
-      })
+      scrollBottom();
+      $('#new_message')[0].reset();      
+    })
     .fail(function(data){
       alert('エラーが発生したためメッセージは送信できませんでした。');
     })
     .always(function(data){
       $('.submit-btn').prop('disabled', false);　//ここで解除している
     })
-  })   
+  })
+  
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data('id');
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+        })
+
+      .done(function(messages) {
+        var HTML = '';
+        messages.forEach(function(message){
+          HTML = buildHTML(message);
+          $('.messages').append(HTML);
+          scrollBottom();
+        })
+      })
+      .fail(function() {
+        alert('error');
+      })
+    }
+  }
+  setInterval(reloadMessages, 5000);   
 });
