d88eff270e13c5c5f4efdd3fba0226f1a78bf4e3
yusuketono <yusuke.tono@di-v.co.jp>
2019-08-15 14:39:59 +0900

    updata

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -31,7 +31,6 @@ $(document).on('turbolinks:load', function() {
 
   $('#new_message').on(`submit`,function(e){
     e.preventDefault();
-    console.log(this)
     var message = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
